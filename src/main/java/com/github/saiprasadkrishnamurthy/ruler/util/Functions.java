740341cd2747073dae12e08f7021fb82b8967e0d
saiprasadkrishnamurthy <saiprasad.krishnamurthy@sita.aero>
2020-07-19 19:06:20 +0530

    A few fixes and removed restriction rules

---
+++ src/main/java/com/github/saiprasadkrishnamurthy/ruler/util/Functions.java
@@ -0,0 +1,7 @@
+package com.github.saiprasadkrishnamurthy.ruler.util;
+
+public class Functions {
+    public static void foo(String ruleName) {
+        System.out.println(" --- From foo --- " + ruleName);
+    }
+}
